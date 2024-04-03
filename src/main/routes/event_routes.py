7a9894ce4a7ca72996ad375ea5be2e5b757fe6af
Adriel <adriel.fsantos@outlook.com>
2024-04-03 13:55:47 -0300

    feat: Add route for getting event data

--- src/main/routes/event_routes.py
+++ src/main/routes/event_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 
 from src.main.data.event_handler import EventHandler
+from src.main.http_types import http_response
 from src.main.http_types.http_request import HttpRequest
 
 event_route_bp = Blueprint("event_route", __name__)
@@ -11,4 +12,12 @@ def create_event():
     http_request = HttpRequest(body=request.json)
     event_handler = EventHandler()
     http_response = event_handler.register(http_request)
+    return jsonify(http_response.body), http_response.status_code
+
+
+@event_route_bp.route('/events/<event_id>', methods=['GET'])
+def get_event(event_id):
+    event_handler = EventHandler()
+    http_request = HttpRequest(param={"event_id": event_id})
+    http_response = event_handler.find_by_id(http_request)
     return jsonify(http_response.body), http_response.status_code
