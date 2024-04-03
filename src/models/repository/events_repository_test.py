f8cc049182857b1560178093b0f5607e1309010e
Adriel <adriel.fsantos@outlook.com>
2024-04-02 21:44:31 -0300

    feat: Add test for get_event_by_id method

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -15,3 +15,11 @@ def test_insert_event():
     events_repository = EventsRepository()
     response = events_repository.insert_event(event)
     print(response)
+
+
+def test_get_event_by_id():
+    event_id = '419fb37b-84c6-490c-80b3-9ca51843fea1'
+
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id)
+    print(response)
