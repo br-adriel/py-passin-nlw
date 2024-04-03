b30423ce52c3b16f69b36be137e234578607e940
Adriel <adriel.fsantos@outlook.com>
2024-04-02 21:15:43 -0300

    feat: Add test for insert_event method

---
+++ src/models/repository/events_repository_test.py
@@ -0,0 +1,17 @@
+from src.models.repository.events_repository import EventsRepository
+from src.models.settings.connection import db_connection_handler
+
+db_connection_handler.connect_to_db()
+
+
+def test_insert_event():
+    event = {
+        "uuid": "419fb37b-84c6-490c-80b3-9ca51843fea1",
+        "title": "Evento 1",
+        "maximum_attendees": 50,
+        "slug": "meu-slug",
+    }
+
+    events_repository = EventsRepository()
+    response = events_repository.insert_event(event)
+    print(response)
