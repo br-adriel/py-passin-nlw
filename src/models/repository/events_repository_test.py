3df1510176e395e419b168e87640fa388363003b
Adriel <adriel.fsantos@outlook.com>
2024-04-02 22:29:51 -0300

    refactor: Update test event id

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -6,7 +6,7 @@ db_connection_handler.connect_to_db()
 
 def test_insert_event():
     event = {
-        "uuid": "419fb37b-84c6-490c-80b3-9ca51843fea1",
+        "uuid": "1",
         "title": "Evento 1",
         "maximum_attendees": 50,
         "slug": "meu-slug",
@@ -18,7 +18,7 @@ def test_insert_event():
 
 
 def test_get_event_by_id():
-    event_id = '419fb37b-84c6-490c-80b3-9ca51843fea1'
+    event_id = '1'
 
     events_repository = EventsRepository()
     response = events_repository.get_event_by_id(event_id)
