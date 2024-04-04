13817c0fffe23e82be9800874ebf26e9b9ff8815
Adriel <adriel.fsantos@outlook.com>
2024-04-03 23:59:40 -0300

    feat: Improve error handling in routes

--- src/main/routes/attendee_routes.py
+++ src/main/routes/attendee_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 
 from src.data.attendees_handler import AttendeeHandler
+from src.errors.error_handler import handle_error
 from src.http_types.http_request import HttpRequest
 
 attendee_route_bp = Blueprint("attendee_route", __name__)
@@ -8,23 +9,37 @@ attendee_route_bp = Blueprint("attendee_route", __name__)
 
 @attendee_route_bp.route('/events/<event_id>/register', methods=['POST'])
 def create_attendee(event_id):
-    http_request = HttpRequest(param={'event_id': event_id}, body=request.json)
-    attendee_handler = AttendeeHandler()
-    http_response = attendee_handler.register(http_request)
-    return jsonify(http_response.body), http_response.status_code
+    try:
+        http_request = HttpRequest(
+            param={'event_id': event_id}, body=request.json)
+        attendee_handler = AttendeeHandler()
+        http_response = attendee_handler.register(http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_error(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 
 @attendee_route_bp.route('/attendees/<attendee_id>/badge', methods=['GET'])
 def get_attendee_badge(attendee_id):
-    http_request = HttpRequest(param={'attendee_id': attendee_id})
-    attendee_handler = AttendeeHandler()
-    http_response = attendee_handler.find_attendee_badge(http_request)
-    return jsonify(http_response.body), http_response.status_code
+    try:
+        http_request = HttpRequest(param={'attendee_id': attendee_id})
+        attendee_handler = AttendeeHandler()
+        http_response = attendee_handler.find_attendee_badge(http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_error(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 
 @attendee_route_bp.route('/events/<event_id>/attendees', methods=['GET'])
 def get_event_attendees(event_id):
-    http_request = HttpRequest(param={'event_id': event_id})
-    attendee_handler = AttendeeHandler()
-    http_response = attendee_handler.find_attendees_from_event(http_request)
-    return jsonify(http_response.body), http_response.status_code
+    try:
+        http_request = HttpRequest(param={'event_id': event_id})
+        attendee_handler = AttendeeHandler()
+        http_response = attendee_handler.find_attendees_from_event(
+            http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_error(exception)
+        return jsonify(http_response.body), http_response.status_code
