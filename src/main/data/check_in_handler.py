3a530d7f2fb1feeba312405f8aca1690c15f4201
Adriel <adriel.fsantos@outlook.com>
2024-04-03 23:32:06 -0300

    feat: Add register method on CheckInHandler

--- src/main/data/check_in_handler.py
+++ src/main/data/check_in_handler.py
@@ -1,6 +1,13 @@
+from src.main.http_types.http_request import HttpRequest
+from src.main.http_types.http_response import HttpResponse
 from src.models.repository.check_ins_repository import CheckInsRepository
 
 
 class CheckInHandler:
     def __init__(self) -> None:
         self.__check_ins_repository = CheckInsRepository()
+
+    def register(self, http_request: HttpRequest) -> HttpResponse:
+        check_in_info = http_request.param.get('attendee_id')
+        self.__check_ins_repository.insert_check_in(check_in_info)
+        return HttpResponse(body=None, status_code=201)
