620446932f8781697f2495f6a1ad391137092ea1
Adriel <adriel.fsantos@outlook.com>
2024-04-03 23:12:13 -0300

    feat: Add find_attendee_badge method on AttendeeHandler

--- src/main/data/attendees_handler.py
+++ src/main/data/attendees_handler.py
@@ -28,3 +28,23 @@ class AttendeeHandler:
 
         self.__attendees_repository.insert_attendee(body)
         return HttpResponse(body=None, status_code=201)
+
+    def find_attendee_badge(self, http_request: HttpRequest) -> HttpResponse:
+        attendee_id = http_request.param.get('attendee_id')
+        badge = self.__attendees_repository.get_attendee_badge_by_id(
+            attendee_id
+        )
+
+        if not badge:
+            raise Exception('Participante não encontrado')
+
+        return HttpResponse(
+            body={
+                'badge': {
+                    'name': badge.name,
+                    'email': badge.email,
+                    'eventTitle': badge.title
+                }
+            },
+            status_code=200
+        )
