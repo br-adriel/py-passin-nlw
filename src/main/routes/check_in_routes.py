e3c7d163b9027a7ad16fecae17f39ae96d81089f
Adriel <adriel.fsantos@outlook.com>
2024-04-03 23:38:41 -0300

    fix: Update create_check_in route method to post

--- src/main/routes/check_in_routes.py
+++ src/main/routes/check_in_routes.py
@@ -6,7 +6,7 @@ from src.main.http_types.http_request import HttpRequest
 check_in_route_bp = Blueprint("check_in_route", __name__)
 
 
-@check_in_route_bp.route('/attendees/<attendee_id>/check-in', methods=['GET'])
+@check_in_route_bp.route('/attendees/<attendee_id>/check-in', methods=['POST'])
 def create_check_in(attendee_id):
     check_in_handler = CheckInHandler()
     http_request = HttpRequest(param={'attendee_id': attendee_id})
